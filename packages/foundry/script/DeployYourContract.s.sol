41ad2bee3d243e94efd519ae6bd9d6ef9775aadf
Jacob Homanics <homanicsjake@gmail.com>
2025-04-24 23:31:37 -0400

    added the missing deploy scripts

--- packages/foundry/script/DeployYourContract.s.sol
+++ packages/foundry/script/DeployYourContract.s.sol
@@ -25,6 +25,41 @@ import "../contracts/events/Manila.sol";
 import "../contracts/events/Dubai.sol";
 import "../contracts/events/DarEsSalaam.sol";
 import "../contracts/events/Kigali.sol";
+import "../contracts/events/Abuja.sol";
+import "../contracts/events/AddisAbaba.sol";
+import "../contracts/events/AngelesCity.sol";
+import "../contracts/events/Austin.sol";
+import "../contracts/events/BalangaCity.sol";
+import "../contracts/events/Bangkok.sol";
+import "../contracts/events/Budapest.sol";
+import "../contracts/events/Cebu.sol";
+import "../contracts/events/Cebu2.sol";
+import "../contracts/events/Cebu3.sol";
+import "../contracts/events/Dasmarias.sol";
+import "../contracts/events/DavaoCity.sol";
+import "../contracts/events/Delhi.sol";
+import "../contracts/events/Enugu.sol";
+import "../contracts/events/Haripur.sol";
+import "../contracts/events/Istanbul.sol";
+import "../contracts/events/Jakarta.sol";
+import "../contracts/events/Kampala.sol";
+import "../contracts/events/Kisumu.sol";
+import "../contracts/events/Kyiv.sol";
+import "../contracts/events/Lagos.sol";
+import "../contracts/events/LegazpieCity.sol";
+import "../contracts/events/Lisbon.sol";
+import "../contracts/events/MakatiCity.sol";
+import "../contracts/events/MalolosCity.sol";
+import "../contracts/events/MexicoCity.sol";
+import "../contracts/events/NagaCity.sol";
+import "../contracts/events/PanabuCity.sol";
+import "../contracts/events/Rome.sol";
+import "../contracts/events/SaoPaulo.sol";
+import "../contracts/events/Seoul.sol";
+import "../contracts/events/Singapore.sol";
+import "../contracts/events/Tagum.sol";
+import "../contracts/events/Zanzibar.sol";
+import "../contracts/events/Zug.sol";
 import {PRBMathSD59x18 as P} from "prb-math/PRBMathSD59x18.sol";
 
 contract DeployYourContract is ScaffoldETHDeploy {
@@ -302,6 +337,359 @@ contract DeployYourContract is ScaffoldETHDeploy {
             "Kigali",
             "KIGALI",
             "ipfs://bafybeiagjmwqdurdhmymdur3p3uwuefp67bzwliehlcxyx6lbau3qcvgje/027-base-batch-workshop-kigali.json",
+            admins,
+            minters,
+            eas,
+            schemaUID
+        );
+
+        // Add all the missing event contracts
+        Abuja abuja = new Abuja(
+            "Abuja",
+            "ABUJA",
+            "ipfs://bafybeiagjmwqdurdhmymdur3p3uwuefp67bzwliehlcxyx6lbau3qcvgje/001-base-batch-workshop-abuja.json",
+            admins,
+            minters,
+            eas,
+            schemaUID
+        );
+
+        AddisAbaba addisAbaba = new AddisAbaba(
+            "AddisAbaba",
+            "ADDISABABA",
+            "ipfs://bafybeiagjmwqdurdhmymdur3p3uwuefp67bzwliehlcxyx6lbau3qcvgje/003-base-batch-workshop-addis-ababa.json",
+            admins,
+            minters,
+            eas,
+            schemaUID
+        );
+
+        AngelesCity angelesCity = new AngelesCity(
+            "AngelesCity",
+            "ANGELESCITY",
+            "ipfs://bafybeiagjmwqdurdhmymdur3p3uwuefp67bzwliehlcxyx6lbau3qcvgje/004-base-batch-workshop-angeles-city.json",
+            admins,
+            minters,
+            eas,
+            schemaUID
+        );
+
+        Austin austin = new Austin(
+            "Austin",
+            "AUSTIN",
+            "ipfs://bafybeiagjmwqdurdhmymdur3p3uwuefp67bzwliehlcxyx6lbau3qcvgje/005-base-batch-workshop-austin.json",
+            admins,
+            minters,
+            eas,
+            schemaUID
+        );
+
+        BalangaCity balangaCity = new BalangaCity(
+            "BalangaCity",
+            "BALANGACITY",
+            "ipfs://bafybeiagjmwqdurdhmymdur3p3uwuefp67bzwliehlcxyx6lbau3qcvgje/006-base-batch-workshop-balanga-city.json",
+            admins,
+            minters,
+            eas,
+            schemaUID
+        );
+
+        Bangkok bangkok = new Bangkok(
+            "Bangkok",
+            "BANGKOK",
+            "ipfs://bafybeiagjmwqdurdhmymdur3p3uwuefp67bzwliehlcxyx6lbau3qcvgje/008-base-batch-workshop-bangkok.json",
+            admins,
+            minters,
+            eas,
+            schemaUID
+        );
+
+        Budapest budapest = new Budapest(
+            "Budapest",
+            "BUDAPEST",
+            "ipfs://bafybeiagjmwqdurdhmymdur3p3uwuefp67bzwliehlcxyx6lbau3qcvgje/009-base-batch-workshop-budapest.json",
+            admins,
+            minters,
+            eas,
+            schemaUID
+        );
+
+        Cebu cebu = new Cebu(
+            "Cebu",
+            "CEBU",
+            "ipfs://bafybeiagjmwqdurdhmymdur3p3uwuefp67bzwliehlcxyx6lbau3qcvgje/015-base-batch-workshop-cebu.json",
+            admins,
+            minters,
+            eas,
+            schemaUID
+        );
+
+        Cebu2 cebu2 = new Cebu2(
+            "Cebu2",
+            "CEBU2",
+            "ipfs://bafybeiagjmwqdurdhmymdur3p3uwuefp67bzwliehlcxyx6lbau3qcvgje/013-base-batch-workshop-cebu-2.json",
+            admins,
+            minters,
+            eas,
+            schemaUID
+        );
+
+        Cebu3 cebu3 = new Cebu3(
+            "Cebu3",
+            "CEBU3",
+            "ipfs://bafybeiagjmwqdurdhmymdur3p3uwuefp67bzwliehlcxyx6lbau3qcvgje/014-base-batch-workshop-cebu-3.json",
+            admins,
+            minters,
+            eas,
+            schemaUID
+        );
+
+        Dasmarias dasmarias = new Dasmarias(
+            "Dasmarias",
+            "DASMARIAS",
+            "ipfs://bafybeiagjmwqdurdhmymdur3p3uwuefp67bzwliehlcxyx6lbau3qcvgje/018-base-batch-workshop-dasmarias.json",
+            admins,
+            minters,
+            eas,
+            schemaUID
+        );
+
+        DavaoCity davaoCity = new DavaoCity(
+            "DavaoCity",
+            "DAVAOCITY",
+            "ipfs://bafybeiagjmwqdurdhmymdur3p3uwuefp67bzwliehlcxyx6lbau3qcvgje/019-base-batch-workshop-davao-city.json",
+            admins,
+            minters,
+            eas,
+            schemaUID
+        );
+
+        Delhi delhi = new Delhi(
+            "Delhi",
+            "DELHI",
+            "ipfs://bafybeiagjmwqdurdhmymdur3p3uwuefp67bzwliehlcxyx6lbau3qcvgje/020-base-batch-workshop-delhi.json",
+            admins,
+            minters,
+            eas,
+            schemaUID
+        );
+
+        Enugu enugu = new Enugu(
+            "Enugu",
+            "ENUGU",
+            "ipfs://bafybeiagjmwqdurdhmymdur3p3uwuefp67bzwliehlcxyx6lbau3qcvgje/021-base-batch-workshop-enugu.json",
+            admins,
+            minters,
+            eas,
+            schemaUID
+        );
+
+        Haripur haripur = new Haripur(
+            "Haripur",
+            "HARIPUR",
+            "ipfs://bafybeiagjmwqdurdhmymdur3p3uwuefp67bzwliehlcxyx6lbau3qcvgje/022-base-batch-workshop-haripur.json",
+            admins,
+            minters,
+            eas,
+            schemaUID
+        );
+
+        Istanbul istanbul = new Istanbul(
+            "Istanbul",
+            "ISTANBUL",
+            "ipfs://bafybeiagjmwqdurdhmymdur3p3uwuefp67bzwliehlcxyx6lbau3qcvgje/024-base-batch-workshop-istanbul.json",
+            admins,
+            minters,
+            eas,
+            schemaUID
+        );
+
+        Jakarta jakarta = new Jakarta(
+            "Jakarta",
+            "JAKARTA",
+            "ipfs://bafybeiagjmwqdurdhmymdur3p3uwuefp67bzwliehlcxyx6lbau3qcvgje/025-base-batch-workshop-jakarta.json",
+            admins,
+            minters,
+            -6140370000, // latitude in nanodegrees (Jakarta is at approximately -6.1403°)
+            106815500000, // longitude in nanodegrees (Jakarta is at approximately 106.8155°)
+            eas,
+            schemaUID
+        );
+
+        Kampala kampala = new Kampala(
+            "Kampala",
+            "KAMPALA",
+            "ipfs://bafybeiagjmwqdurdhmymdur3p3uwuefp67bzwliehlcxyx6lbau3qcvgje/026-base-batch-workshop-kampala.json",
+            admins,
+            minters,
+            eas,
+            schemaUID
+        );
+
+        Kisumu kisumu = new Kisumu(
+            "Kisumu",
+            "KISUMU",
+            "ipfs://bafybeiagjmwqdurdhmymdur3p3uwuefp67bzwliehlcxyx6lbau3qcvgje/028-base-batch-workshop-kisumu.json",
+            admins,
+            minters,
+            eas,
+            schemaUID
+        );
+
+        Kyiv kyiv = new Kyiv(
+            "Kyiv",
+            "KYIV",
+            "ipfs://bafybeiagjmwqdurdhmymdur3p3uwuefp67bzwliehlcxyx6lbau3qcvgje/029-base-batch-workshop-kyiv.json",
+            admins,
+            minters,
+            eas,
+            schemaUID
+        );
+
+        Lagos lagos = new Lagos(
+            "Lagos",
+            "LAGOS",
+            "ipfs://bafybeiagjmwqdurdhmymdur3p3uwuefp67bzwliehlcxyx6lbau3qcvgje/030-base-batch-workshop-lagos.json",
+            admins,
+            minters,
+            eas,
+            schemaUID
+        );
+
+        LegazpieCity legazpieCity = new LegazpieCity(
+            "LegazpieCity",
+            "LEGAZPIECITY",
+            "ipfs://bafybeiagjmwqdurdhmymdur3p3uwuefp67bzwliehlcxyx6lbau3qcvgje/031-base-batch-workshop-legazpie-city.json",
+            admins,
+            minters,
+            eas,
+            schemaUID
+        );
+
+        Lisbon lisbon = new Lisbon(
+            "Lisbon",
+            "LISBON",
+            "ipfs://bafybeiagjmwqdurdhmymdur3p3uwuefp67bzwliehlcxyx6lbau3qcvgje/032-base-batch-workshop-lisbon.json",
+            admins,
+            minters,
+            eas,
+            schemaUID
+        );
+
+        MakatiCity makatiCity = new MakatiCity(
+            "MakatiCity",
+            "MAKATICITY",
+            "ipfs://bafybeiagjmwqdurdhmymdur3p3uwuefp67bzwliehlcxyx6lbau3qcvgje/033-base-batch-workshop-makati-city.json",
+            admins,
+            minters,
+            eas,
+            schemaUID
+        );
+
+        MalolosCity malolosCity = new MalolosCity(
+            "MalolosCity",
+            "MALOLOSCITY",
+            "ipfs://bafybeiagjmwqdurdhmymdur3p3uwuefp67bzwliehlcxyx6lbau3qcvgje/034-base-batch-workshop-malolos-city.json",
+            admins,
+            minters,
+            eas,
+            schemaUID
+        );
+
+        MexicoCity mexicoCity = new MexicoCity(
+            "MexicoCity",
+            "MEXICOCITY",
+            "ipfs://bafybeiagjmwqdurdhmymdur3p3uwuefp67bzwliehlcxyx6lbau3qcvgje/036-base-batch-workshop-mexico-city.json",
+            admins,
+            minters,
+            eas,
+            schemaUID
+        );
+
+        NagaCity nagaCity = new NagaCity(
+            "NagaCity",
+            "NAGACITY",
+            "ipfs://bafybeiagjmwqdurdhmymdur3p3uwuefp67bzwliehlcxyx6lbau3qcvgje/038-base-batch-workshop-naga-city.json",
+            admins,
+            minters,
+            eas,
+            schemaUID
+        );
+
+        PanabuCity panabuCity = new PanabuCity(
+            "PanabuCity",
+            "PANABUCITY",
+            "ipfs://bafybeiagjmwqdurdhmymdur3p3uwuefp67bzwliehlcxyx6lbau3qcvgje/041-base-batch-workshop-panabu-city.json",
+            admins,
+            minters,
+            eas,
+            schemaUID
+        );
+
+        Rome rome = new Rome(
+            "Rome",
+            "ROME",
+            "ipfs://bafybeiagjmwqdurdhmymdur3p3uwuefp67bzwliehlcxyx6lbau3qcvgje/043-base-batch-workshop-rome.json",
+            admins,
+            minters,
+            eas,
+            schemaUID
+        );
+
+        SaoPaulo saoPaulo = new SaoPaulo(
+            "SaoPaulo",
+            "SAOPAULO",
+            "ipfs://bafybeiagjmwqdurdhmymdur3p3uwuefp67bzwliehlcxyx6lbau3qcvgje/046-base-batch-workshop-so-paulo.json",
+            admins,
+            minters,
+            eas,
+            schemaUID
+        );
+
+        Seoul seoul = new Seoul(
+            "Seoul",
+            "SEOUL",
+            "ipfs://bafybeiagjmwqdurdhmymdur3p3uwuefp67bzwliehlcxyx6lbau3qcvgje/044-base-batch-workshop-seoul.json",
+            admins,
+            minters,
+            eas,
+            schemaUID
+        );
+
+        Singapore singapore = new Singapore(
+            "Singapore",
+            "SINGAPORE",
+            "ipfs://bafybeiagjmwqdurdhmymdur3p3uwuefp67bzwliehlcxyx6lbau3qcvgje/045-base-batch-workshop-singapore.json",
+            admins,
+            minters,
+            eas,
+            schemaUID
+        );
+
+        Tagum tagum = new Tagum(
+            "Tagum",
+            "TAGUM",
+            "ipfs://bafybeiagjmwqdurdhmymdur3p3uwuefp67bzwliehlcxyx6lbau3qcvgje/048-base-batch-workshop-tagum.json",
+            admins,
+            minters,
+            eas,
+            schemaUID
+        );
+
+        Zanzibar zanzibar = new Zanzibar(
+            "Zanzibar",
+            "ZANZIBAR",
+            "ipfs://bafybeiagjmwqdurdhmymdur3p3uwuefp67bzwliehlcxyx6lbau3qcvgje/050-base-batch-workshop-zanzibar.json",
+            admins,
+            minters,
+            eas,
+            schemaUID
+        );
+
+        Zug zug = new Zug(
+            "Zug",
+            "ZUG",
+            "ipfs://bafybeiagjmwqdurdhmymdur3p3uwuefp67bzwliehlcxyx6lbau3qcvgje/051-base-batch-workshop-zug.json",
             admins,
             minters,
             eas,
